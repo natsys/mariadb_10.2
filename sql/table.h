7db0323e53f7e2e6147a449d118a704aaf8fb029
Daniel Fiala <freelancer@meave.biz>
2016-08-26 11:57:17 +0200

    Fixed dump of SYSTEM_TIME (used eg. in views).
    	* Not printed TIMESTAMP, it is printed by item.
    	* If time conditions were added to where clause, then
    	  SYSTEM_TIME isn't printed.

--- sql/table.h
+++ sql/table.h
@@ -1782,6 +1782,7 @@ struct system_versioning_for_select
 {
   enum for_system_time_type type;
   Item *start, *end;
+  bool is_moved_to_where;
 
   void init(
     const enum for_system_time_type t=FOR_SYSTEM_TIME_UNSPECIFIED,
@@ -1791,6 +1792,7 @@ struct system_versioning_for_select
     type= t;
     start= s;
     end= e;
+    is_moved_to_where= false;
   }
 };
 
