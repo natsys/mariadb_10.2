186e646ed8d56a26fd869952adc7942f6ea4438e
Aleksey Midenkov <midenok@gmail.com>
2016-09-15 01:29:33 +0000

    Style: removed cosmetics

--- sql/item.cc
+++ sql/item.cc
@@ -4324,6 +4324,7 @@ void Item_copy_decimal::copy()
 /* ARGSUSED */
 bool Item::fix_fields(THD *thd, Item **ref)
 {
+
   // We do not check fields which are fixed during construction
   DBUG_ASSERT(fixed == 0 || basic_const_item());
   fixed= 1;
