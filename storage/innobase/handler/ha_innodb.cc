c196fbc9a745738916a92fbd76b7e061a75d6f1d
Eugene Kosov <claprix@yandex.ru>
2017-12-26 16:05:43 +0300

    fixes

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -6556,6 +6556,10 @@ no_such_table:
 		}
 	}
 
+	if (table && m_prebuilt->table) {
+		ut_ad(table->versioned() == m_prebuilt->table->versioned());
+	}
+
 	info(HA_STATUS_NO_LOCK | HA_STATUS_VARIABLE | HA_STATUS_CONST | HA_STATUS_OPEN);
 	DBUG_RETURN(0);
 }
