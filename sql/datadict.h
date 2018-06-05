3b9be1c72fe649627b8410fda9ba92bfcb8c9190
Nikita Malyavin <nikitamalyavin@gmail.com>
2018-06-06 05:02:51 +1000

    MDEV-15966: Behavior for TRUNCATE versioned table is not documented and not covered by tests
    * add error for truncation of versioned tables: `ER_TRUNCATE_ILLEGAL_VERS`
    * extract reading `extra2` section in function `dd_read_extra2`
    * update `dd_frm_type` and `ha_table_exists` signatures: add new parameter `is_versioned`
    
    tests run: main/create, parts/*, versioning/*
    
    [fixes #261]

--- sql/datadict.h
+++ sql/datadict.h
@@ -30,6 +30,21 @@ enum Table_type
   TABLE_TYPE_VIEW
 };
 
+struct extra2_fields
+{
+  const uchar *version;
+  size_t version_len;
+  const uchar *options;
+  size_t options_len;
+  const uchar *part_engine;
+  size_t part_engine_len;
+  const uchar *gis;
+  size_t gis_len;
+  const uchar *system_period;
+  const uchar *field_flags;
+  size_t field_flags_len;
+};
+
 /*
   Take extra care when using dd_frm_type() - it only checks the .frm file,
   and it won't work for any engine that supports discovery.
@@ -39,15 +54,16 @@ enum Table_type
 */
 
 enum Table_type dd_frm_type(THD *thd, char *path, LEX_CSTRING *engine_name,
-                            bool *is_sequence);
+                            bool *is_sequence, bool *is_versioned);
 
 static inline bool dd_frm_is_view(THD *thd, char *path)
 {
   bool not_used2;
-  return dd_frm_type(thd, path, NULL, &not_used2) == TABLE_TYPE_VIEW;
+  return dd_frm_type(thd, path, NULL, &not_used2, NULL) == TABLE_TYPE_VIEW;
 }
 
 bool dd_recreate_table(THD *thd, const char *db, const char *table_name,
                        const char *path = NULL);
 
+bool dd_read_extra2(const uchar *frm_image, size_t len, extra2_fields *fields);
 #endif // DATADICT_INCLUDED
